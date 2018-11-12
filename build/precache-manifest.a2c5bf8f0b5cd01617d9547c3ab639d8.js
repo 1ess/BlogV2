f2552402cf99c5d37995b861e6e8fe636d32f57a
GodzzZZZ <godzzzzz@qq.com>
2018-11-12 11:40:07 +0800

    Update Production Files

---
+++ build/precache-manifest.a2c5bf8f0b5cd01617d9547c3ab639d8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "d129737af57933606a1137bc364ecc34",
+    "url": "/static/media/wechat.d129737a.jpg"
+  },
+  {
+    "revision": "22e7158b3170005093826e27b22db954",
+    "url": "/static/media/message-img.22e7158b.jpg"
+  },
+  {
+    "revision": "5f65e21e587ea457d11cd4f31f898b6a",
+    "url": "/static/media/icon.5f65e21e.jpg"
+  },
+  {
+    "revision": "b39756dbf0246cea6c732f58fb257f7d",
+    "url": "/static/media/footer.b39756db.png"
+  },
+  {
+    "revision": "3072a3ec3c6e3be046d1c60c30036bc6",
+    "url": "/static/media/404.3072a3ec.gif"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8674d64ef53250997dd4",
+    "url": "/static/js/main.8674d64e.chunk.js"
+  },
+  {
+    "revision": "efb27068cb785d0464ff",
+    "url": "/static/js/1.efb27068.chunk.js"
+  },
+  {
+    "revision": "8674d64ef53250997dd4",
+    "url": "/static/css/main.9f3531b0.chunk.css"
+  },
+  {
+    "revision": "640b6d55ef4746821d42bc06a934a769",
+    "url": "/index.html"
+  }
+];
