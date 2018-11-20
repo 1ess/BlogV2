07d3e195db22d73c2b644c9f0b629293d0a0422d
GodzzZZZ <godzzzzz@qq.com>
2018-11-20 20:40:24 +0800

    Update kernel panic

--- src/KernelPanic/KernelPanic.js
+++ src/KernelPanic/KernelPanic.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import PanicItem from '../PanicItem/PanicItem';
-
+import Header from '../Header/Header';
 import axios from 'axios';
 
 import '../App.css';
@@ -25,6 +25,7 @@ export default class KernelPanic extends Component {
     }
 
     componentDidMount() {
+        document.title = " ❤️ Kernel Panic";
         const self = this;
         axios({
             method: 'get',
@@ -74,50 +75,53 @@ export default class KernelPanic extends Component {
 
     render() {
         const { items, symbol, isLoading, error }= this.state;
-        return (
+        return (                
             <div className={`app-container`}>
-                <div className={`app-content`}>
-                    <div className={`container`}>
-                        <div className={`columns has-equal-heights reverse`}>
-                            <div className={`column is-8 is-offset-2`}>
-                                <div className={`section-header`}>
-                                    <h2 className={`title has-text-centered is-spaced cms-title`}> 
-                                        Content Management 
-                                        <span className={`spacer is-horizontal is-05`}> </span>
-                                        <span className={`tag is-warning is-uppercase`}>Beta</span>
-                                    </h2>
-                                    {
-                                        isLoading 
-                                        ? 
-                                        <p className={`has-text-centered is-loading`}>Loading ...</p>
-                                        :
-                                        error 
-                                        ?
-                                        <p className={`has-text-centered is-loading`}>加载失败</p>
-                                        :
-                                        <ul className={`numbered-list`}>
-                                            {
-                                                items.map((item, index) => (
-                                                    <PanicItem key={item.id} index={item.tag} title={item.title} date={item.date} summary={item.summary}/>
-                                                ))
-                                            }
-                                        </ul>
-                                    }
-                                </div>
-                            </div>                            
+                <Header symbol={`❤️`} selectedIndex={3} />
+                <div className={`app-content has-background`}>
+                    <div className={`hero is-medium`}>
+                        <div className={`hero-body kernel-panic`}>
+                            <div className={`columns has-equal-heights reverse`}>
+                                <div className={`column is-8 is-offset-2`}>
+                                    <div className={`section-header`}>
+                                        <h2 className={`title has-text-centered is-spaced`}> 
+                                            Content Management 
+                                            <span className={`spacer is-horizontal is-05`}> </span>
+                                            <span className={`tag is-warning is-uppercase`}>Beta</span>
+                                        </h2>
+                                        {
+                                            isLoading 
+                                            ? 
+                                            <p className={`has-text-centered is-loading`}>Loading ...</p>
+                                            :
+                                            error 
+                                            ?
+                                            <p className={`has-text-centered is-loading`}>加载失败</p>
+                                            :
+                                            <ul className={`numbered-list`}>
+                                                {
+                                                    items.map((item, index) => (
+                                                        <PanicItem key={item.id} index={item.tag} title={item.title} date={item.date} summary={item.summary}/>
+                                                    ))
+                                                }
+                                            </ul>
+                                        }
+                                    </div>
+                                </div>                            
+                            </div>
+                            <div className={`columns has-equal-heights reverse`}>
+                                {
+                                    isLoading 
+                                    || 
+                                    (
+                                    <div className={`column is-2 is-offset-8`}>
+                                        <button onClick={this.handleClick} className={`button is-warning`} ><span className ="emoji" role ="img" aria-label="heart">{symbol}</span> Create.</button>
+                                    </div>
+                                    )
+                                }
+                            </div>
                         </div>
-                        <div className={`columns has-equal-heights reverse`}>
-                            {
-                                isLoading 
-                                || 
-                                (
-                                <div className={`column is-2 is-offset-8`}>
-                                    <button onClick={this.handleClick} className={`button is-warning`} ><span className ="emoji" role ="img" aria-label="heart">{symbol}</span> Create.</button>
-                                </div>
-                                )
-                            }
-                        </div>
-                    </div>
+                    </div>                        
                 </div>
             </div>
         );
